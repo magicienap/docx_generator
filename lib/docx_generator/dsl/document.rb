b3af9b20726c5fa0d26b22823042cf25a5313fde
Antoine Proulx <proulx.antoine@gmail.com>
2013-07-13 15:45:32 -0400

    Documentation of the Document and the Paragraph classes.

--- lib/docx_generator/dsl/document.rb
+++ lib/docx_generator/dsl/document.rb
@@ -1,24 +1,34 @@
 module DocxGenerator
   module DSL
+    # Represent the docx document.
     class Document
+      # Filename of the document (without the docx extension).
       attr_reader :filename
     
+      # Create a new docx document.
+      # @param filename [String] The filename of the docx file, without the docx extension.
       def initialize(filename, &block)
         @filename = filename + ".docx"
         @objects = [] # It contains all the DSL elements
         yield self if block
       end
       
+      # Save the docx document to the target location.
       def save
         generate_archive(generate_content_types, generate_rels, generate_document)
       end
 
+      # Add a new paragraph to the document.
+      # @param options [Hash] Formatting options for the paragraph. See the full list in DocxGenerator::DSL::Paragraph.
       def paragraph(options = {}, &block)
         par = DocxGenerator::DSL::Paragraph.new(options)
         yield par if block
         @objects << par
       end
 
+      # Add other objects to the document.
+      # @param objects [Object] Objects (like paragraphs).
+      # @return [DocxGenerator::DSL::Document] The document object.
       def add(*objects)
         objects.each do |object|
           @objects << object
--- lib/docx_generator/dsl/paragraph.rb
+++ lib/docx_generator/dsl/paragraph.rb
@@ -1,26 +1,39 @@
 module DocxGenerator
   module DSL
+    # Represent a paragraph with formatting options
     class Paragraph
+      # Create a new paragraph with the formatting options specified.
+      # The formatting properties can be passed with a Hash or they could be set by calling the methods on the object (either in the block or not).
+      # @param options [Hash] Formatting options.
+      # @option options [Boolean] alignment The alignment of the paragraph. See the specification for the complete list.
       def initialize(options = {}, &block)
         @objects = []
         @options = options
         yield self if block
       end
 
+      # Set the alignment of the paragraph. See the specification for more details.
+      # @param value [String] The alignment of the paragraph. See the specification for the complete list.
       def alignment(value)
         @options[:alignment] = value
       end
 
+      # Prevent the addition of a space between two text fragments.
       def no_space
         @objects << DocxGenerator::Word::Extensions::NoSpace.new
       end
 
+      # Add a new text fragment to the paragraph.
+      # @param text_fragment [String] The text fragment.
+      # @param options [Hash] Formatting options for the text fragment. See the full list in DocxGenerator::DSL::Text.
       def text(text_fragment, options = {}, &block)
         text_object = DocxGenerator::DSL::Text.new(text_fragment, options)
         yield text_object if block
         @objects << text_object
       end
 
+      # Generate the XML element objects.
+      # @return [DocxGenerator::Word::Paragraph] A Word::Paragraph object representing the paragraph.
       def generate
         text_fragments = generate_text_fragments
         options = generate_paragraph_options
@@ -31,6 +44,9 @@ module DocxGenerator
         end
       end
 
+      # Add other objects to the paragraph.
+      # @param objects [Object] Objects (like text fragments).
+      # @return [DocxGenerator::DSL::Paragraph] The paragraph object.
       def add(*objects)
         objects.each do |object|
           @objects << object
