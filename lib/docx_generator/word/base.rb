aeecfd94b7ff90e5bca411cc3146ef5eaa20c160
Antoine Proulx <proulx.antoine@gmail.com>
2013-07-20 17:11:03 -0400

    Standardization of the lib/docx_generator/word/base.rb file.

--- lib/docx_generator/word/base.rb
+++ lib/docx_generator/word/base.rb
@@ -2,15 +2,14 @@ module DocxGenerator
   module Word
     # Represent the `w:document` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Document < Element
-      # Create a new `w:document` element.
-      # @param attributes [Hash] The attributes of the XML element.
-      #   In the case of the `w:document` element, the only attribute should be `w:conformance`. If ommited, the value is `transitional`. If not, the two accepted values are `strict` and `transitional`.
+      # Create a new `w:document` XML element.
+      # @param attributes [Hash] The attributes of the XML element. Check the specification of the `w:document` element for the possible attributes.
       # @param content [Array] An array of the children of the XML element (other XML elements).
       def initialize(attributes = {}, content = [])
         super("w:document", attributes, content)
       end
     end
-    
+
     # Represent the `w:body` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Body < Element
       # Create a new `w:body` XML element.
@@ -19,10 +18,10 @@ module DocxGenerator
         super("w:body", {}, content)
       end
     end
-    
+
     # Represent the `w:p` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Paragraph < Element
-      # Create a new `w:p` element.
+      # Create a new `w:p` XML element.
       # @param attributes [Hash] The attributes of the XML element. Check the specification of the `w:p` element for the possible attributes.
       # @param content [Array] An array of the children of the XML element (other XML elements).
       def initialize(attributes = {}, content = [])
@@ -41,14 +40,14 @@ module DocxGenerator
 
     # Represent the `w:r` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Run < Element
-      # Create a new `w:r` element.
+      # Create a new `w:r` XML element.
       # @param attributes [Hash] The attributes of the XML element. Check the specification of the `w:r` element for the possible attributes.
       # @param content [Array] An array of the children of the XML element (other XML elements).
       def initialize(attributes = {}, content = [])
         super("w:r", attributes, content)
       end
     end
-    
+
     # Represent the `w:rPr` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class RunProperties < Element
       # Create a new `w:rPr` XML element.
@@ -57,12 +56,11 @@ module DocxGenerator
         super("w:rPr", {}, content)
       end
     end
-    
+
     # Represent the `w:t` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Text < Element
-      # Create a new `w:t` element.
-      # @param attributes [Hash] The attributes of the XML element.
-      #   In the case of the `w:t` element, the only attribute should be `xml:space`. It means that the content contains significant whitespace. 
+      # Create a new `w:t` XML element.
+      # @param attributes [Hash] The attributes of the XML element. Check the specification of the `w:t` element for the possible attributes.
       # @param content [Array] An array of the children of the XML element (other XML elements).
       def initialize(attributes = {}, content = [])
         super("w:t", attributes, content)
@@ -71,7 +69,7 @@ module DocxGenerator
 
     # Represent the `w:br` element from Office Open XML specification. This class should not be used directly by the users of the library.
     class Break < Element
-      # Create a new `w:br` element.
+      # Create a new `w:br` XML element.
       # @param attributes [Hash] The attributes of the XML element. Check the specification of the `w:br` element for the possible attributes.
       def initialize(attributes = {})
         super("w:br", attributes)
